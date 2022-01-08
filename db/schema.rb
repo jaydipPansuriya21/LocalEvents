89329b610ef5c59f9374687a406a6b356a9e4572
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-01-08 20:47:23 +0530

    rename column name of event_vote table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_08_132620) do
+ActiveRecord::Schema.define(version: 2022_01_08_151329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "event_votes", force: :cascade do |t|
     t.integer "upvote"
-    t.integer "downvot"
+    t.integer "downvote"
     t.integer "viewcount"
     t.bigint "event_id"
     t.datetime "created_at", precision: 6, null: false
