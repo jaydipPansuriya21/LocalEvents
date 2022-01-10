aae0eb52bd81c3c9694fbd7f903d0ed77b143096
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-01-10 21:10:47 +0530

    Event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,13 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def event_analytics
+    begin
+      @event= Event.find(params[:id])
+      @event_vote = @event.event_vote
+    rescue
+      raise "Some exception occure"
+    end
+  end
 end
