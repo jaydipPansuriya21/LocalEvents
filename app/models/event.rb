111e04eb14e37d0f07ab814d3d4b26cd2ac7a07f
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-01-10 22:08:52 +0530

    used delegate method and after_initialize callback

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,11 @@
 class Event < ApplicationRecord
-    has_one :event_vote
+    has_one :event_vote, dependent: :destroy
+    delegate *EventVote::METHODS, to: :event_vote
+
     STATUS_OPTIONS = %w(Pending Rejected InReview)
     validates :status, inclusion:  { in: STATUS_OPTIONS }  
     validates :title, presence: :true
+    after_initialize do 
+        self.build_event_vote if event_vote.nil?
+    end
 end
--- app/models/event_vote.rb
+++ app/models/event_vote.rb
@@ -1,5 +1,6 @@
 class EventVote < ApplicationRecord
     belongs_to :event
+    METHODS = %i(viewcount viewcount= downvote downvote= upvote upvote=)
     validates :viewcount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     validates :upvote, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     validates :downvote, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
