cbfa008c7efda8f142102e659521e4ad8dcdbed8
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-01-12 23:28:10 +0530

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get 'events', to: 'events#index'
   get 'events/event_analytics', to: 'events#event_analytics'
+  post 'events', to: 'events#create'
+  put 'events', to: 'events#update'
+  get 'events/:id', to: 'events#show'
+  post 'events/add_view', to: 'events#add_view'
+  post 'events/add_vote', to: 'events#add_vote'
+  post 'events/remove_vote', to: 'events#remove_vote'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
