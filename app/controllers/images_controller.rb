73473281ebb0f7c4fead5aa6cacb89b0f2f6f7aa
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-01-30 17:15:54 +0530

    Update images_controller.rb

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,7 @@
 class ImagesController <  ApplicationController
     def upload_image
         if params[:image]
-            file_service = AmazonService::FileUpload.new(params[:image]) 
+            file_service = get_file_service_object(params[:image_name])
             image_name = file_service.file_upload
             @image = Image.new(images_path: image_name)
             @image.save_image_information(params)
@@ -12,7 +12,7 @@ class ImagesController <  ApplicationController
 
     def remove_image
         if params[:image_name]
-            file_service = AmazonService::FileUpload.new(params[:image])
+            file_service = get_file_service_object(params[:image_name])
             file_service.file_delete(params[:image_name])
             @image = Image.find_by(images_path: image_name)
             @image.destroy
@@ -20,4 +20,12 @@ class ImagesController <  ApplicationController
             render json: { Error: "Image name is not given"}
         end
     end
+
+    
+    private 
+
+    def get_file_service_object(file)
+        AmazonService::FileUpload.new(file)
+    end
+
 end
