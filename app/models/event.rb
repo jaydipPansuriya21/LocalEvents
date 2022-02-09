c5208e34e353dbca2a4ea5b0c0f6d431bd03b46b
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-02-09 23:17:45 +0530

    change in filter and ordering method

--- app/models/event.rb
+++ app/models/event.rb
@@ -33,19 +33,52 @@ class Event < ApplicationRecord
         elsif params[:order] == "oldest"
             data = fetch_record(filter_hash, :asc) 
         elsif params[:order] == "most_viewed"
-        
+            data = fetch_record(filter_hash, :most_viewed)
         elsif params[:order] == "most_upvoted"
-
+            data = fetch_record(filter_hash, :most_upvoted)
+        elsif params[:order] == "most_downvoted"
+            data = fetch_record(filter_hash, :most_downvoted)
+        elsif params[:order] == "least_viewed"
+            data = fetch_record(filter_hash, :least_viewed)
+        elsif params[:order] == "least_upvoted"
+            data = fetch_record(filter_hash, :least_upvoted)
+        elsif params[:order] == "least_downvoted"
+            data = fetch_record(filter_hash, :least_downvoted)
         end
         data
     end
 
     def self.fetch_record(filter_hash, order)
+        data = do_filter(filter_hash)
+        data = do_order(data, order)
+        data
+    end
+
+    def self.do_order(data, order)
+        if [:desc, :asc].include? order
+            ordered_data = data.order(created_at: order)
+        elsif order == :most_viewed
+            ordered_data = data.joins(:event_vote).order('viewcount desc')
+        elsif order == :most_upvoted
+            ordered_data = data.joins(:event_vote).order('upvote desc')
+        elsif order == :most_downvoted
+            ordered_data = data.joins(:event_vote).order('downvote desc')
+        elsif order == :least_viewed
+            ordered_data = data.joins(:event_vote).order('viewcount')
+        elsif order == :least_upvoted
+            ordered_data = data.joins(:event_vote).order('upvote')
+        elsif order == :least_downvoted
+            ordered_data = data.joins(:event_vote).order('downvote')
+        end
+        ordered_data    
+    end
+
+    def self.do_filter(filter_hash)
         if filter_hash.size.positive?
             # byebug
-            data = Event.where(filter_hash).order(created_at: order)
+            data = Event.where(filter_hash)
         else
-            data = Event.all.order(created_at: order)
+            data = Event.all
         end
         data
     end
