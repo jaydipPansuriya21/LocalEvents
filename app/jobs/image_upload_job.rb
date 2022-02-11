8e6da02c5f33aef941a9401d766030a4f9dd0e3e
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-02-11 22:20:49 +0530

    Update image_upload_job.rb

--- app/jobs/image_upload_job.rb
+++ app/jobs/image_upload_job.rb
@@ -2,6 +2,23 @@ class ImageUploadJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    puts "Image is uploading .."
+    file_path = ENV['STORAGE_LOCATION']+args[0]
+    info_hash = {}
+    info_hash[:id] = args[1]
+    info_hash[:type] = args[2]
+
+    file_service = get_file_service_object(file_path)
+    image_name = file_service.file_upload
+    
+    @image = Image.new(images_path: image_name)
+    @image.save_image_information(info_hash)
+    File.delete(file_path)
   end
+
+  private 
+
+  def get_file_service_object(file_path)
+      AmazonService::FileUpload.new(file_path)
+  end
+
 end
