fc3acc5ea0bc22bda8640dfb8efbcda8de87e61b
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2022-02-11 22:20:43 +0530

    Update images_controller.rb

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,10 +1,10 @@
 class ImagesController <  ApplicationController
     def upload_image
         if params[:image_name]
-            file_service = get_file_service_object(params[:image_name])
-            image_name = file_service.file_upload
-            @image = Image.new(images_path: image_name)
-            @image.save_image_information(params)
+            file_object = params[:image_name]
+            store_file(file_object)
+            ImageUploadJob.perform_later file_object.original_filename, params[:id], params[:type] 
+            render json: { Message: "Image is being uploading in background"}
         else
             render json: { Error: "Image is not given"}
         end
@@ -21,11 +21,12 @@ class ImagesController <  ApplicationController
         end
     end
 
-    
-    private 
-
-    def get_file_service_object(file)
-        AmazonService::FileUpload.new(file)
+    def store_file(file_object)
+        dir = Rails.root.join('public', 'uploads')
+        Dir.mkdir(dir) unless Dir.exist?(dir)
+        File.open(dir.join(file_object.original_filename), 'wb') do |file|
+            file.write(file_object.read)
+        end
     end
 
 end
